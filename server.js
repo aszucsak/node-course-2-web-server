2d1e60269960f344ab6ea4dddec7ac4041beb346
--global <aszucsak@gmail.com>
2018-08-24 22:05:17 +0200

    Projects page added

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    message: 'Project portfolio page here.'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
